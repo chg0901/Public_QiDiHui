d53848c8357b83d8af29fe0420f8e6b46f24d488
YueZhengMeng <804570270@qq.com>
2024-08-03 18:27:19 +0800

    完善了注释

--- rag_full/rag_langchain.py
+++ rag_full/rag_langchain.py
@@ -6,6 +6,9 @@ from langchain_core.documents import Document
 
 
 def resolve_assets(relative_path):
+    """
+    根据相对路径解析出绝对路径
+    """
     return os.path.join(os.path.dirname(__file__), relative_path)
 
 
@@ -17,6 +20,9 @@ faiss_name = resolve_assets("../faiss_index_langchain_full_ernie")
 
 
 def load_dataset():
+    """
+    加载数据集文件
+    """
     book_list = ["Astronomical"]
     json_data = []
     for book_name in book_list:
@@ -26,10 +32,13 @@ def load_dataset():
 
 
 def generate_docs(json_data=None):
+    """
+    将原始数据转换为适用于问答对编码的langchain Document对象
+    """
     if json_data is None:
         json_data = load_dataset()
 
-    # 创建待编码文本集
+    # 创建待编码文档集
     docs = []
     for i in range(len(json_data)):
         # 取出每个问答对的问题和答案
@@ -47,23 +56,34 @@ def generate_docs(json_data=None):
 
 class FaissSearch:
     def __init__(self, path=faiss_name, top_k=5, threshold=10):
+        """
+        初始化函数，用于创建一个检索器对象。
+
+        Args:
+            path (str, optional): FAISS索引文件持久化的路径，默认为'faiss_name'。
+            top_k (int, optional): 返回的相似度最高的k个结果，默认为5。
+            threshold (float, optional): 相似度阈值，只有当相似度大于该阈值时，才会被作为候选结果，默认为10。
+        """
         self.top_k = top_k
         self.threshold = threshold
         self.retriever = get_BM25_retriever(top_k)
 
     def bm25_search(self, real_query):
+        """
+        调用BM25检索器
+        """
         print("开始检索")
         start_time = time.time()
         docs = self.retriever.get_relevant_documents(real_query)
         print("检索结束，耗时：", time.time() - start_time)
         retrieval_results = []
+        score = len(docs)
         for doc in docs:
             # 遍历检索到的文档
             # 把答案从metadata中取出，作为检索结果返回
             # langchain的BM25检索器不返回score，只返回文档
-            # 这里直接用文档的顺序作为评分，靠前的文档评分高
+            # 这里不进行过滤，而是直接用文档的顺序作为评分，靠前的文档评分高
             print(doc)
-            score = len(docs)
             retrieval_results.append(
                 {"content": doc.metadata["answer"], "score": score, "title": doc.page_content,
                  "image": doc.metadata["image"], "source_book": doc.metadata["source_book"],
@@ -74,25 +94,26 @@ class FaissSearch:
         return retrieval_results
 
     def search(self, query, **kwargs):
-        # 定义一个搜索方法，接受一个查询字符串 'query'
-
-        # FunctionAgentWithRetrieval调用时，query中包含了prompt，需要去掉
-        # query_copy = query
-        # real_query = query_copy.replace(prompt, '')
-
-        # 使用我们自己写的stream_run_llm调用时，query中不包含prompt，直接使用
+        """
+        执行检索
+        """
         real_query = query
-
         return self.bm25_search(real_query)
 
 
 def tokenize_chinese(text):
+    """
+    使用jieba进行中文分词
+    """
     # 我也不知道为什么，直接把jieba.lcut作为BM25Retriever.from_defaults的参数，会导致无法pickle序列化
     # 这里重新封装一下，就可以了
     return jieba.lcut(text)
 
 
 def create_BM25_retriever(top_k=5):
+    """
+    基于待编码文档集，创建BM25检索器，分词器使用jieba
+    """
     from langchain_community.retrievers import BM25Retriever
     docs = generate_docs()
     bm25_retriever = BM25Retriever.from_documents(documents=docs, preprocess_func=tokenize_chinese)
@@ -101,6 +122,9 @@ def create_BM25_retriever(top_k=5):
 
 
 def get_BM25_retriever(top_k=5):
+    """
+    获取BM25检索器，如果已经存在则加载，否则创建并持久化
+    """
     import pickle
     if os.path.exists(faiss_name + "/bm25retriever.pkl"):
         bm25_retriever = pickle.load(open(faiss_name + "/bm25retriever.pkl", 'rb'))
@@ -112,19 +136,27 @@ def get_BM25_retriever(top_k=5):
 
 
 def faiss_search_test(top_k=5, threshold=10):
+    """
+    FaissSearch类的测试函数
+    """
     faiss_search = FaissSearch(top_k=top_k, threshold=threshold)
-    results = faiss_search.search("鸟为什么会飞")
+    results = faiss_search.search("天上有多少颗星星")
     for result in results:
         print(result)
 
 
 def bm25_retriever_test(top_k=5):
+    """
+    BM25检索器的测试函数
+    """
     bm25_retriever = get_BM25_retriever(top_k)
-    results = bm25_retriever.get_relevant_documents("鸟为什么会飞")
+    results = bm25_retriever.get_relevant_documents("天上有多少颗星星")
     for result in results:
         print(result)
 
 
 if __name__ == "__main__":
+    # 启动FaissSearch类的测试
     faiss_search_test(top_k=3, threshold=10)
+    # 启动BM25检索器的测试
     bm25_retriever_test(3)
